8e3b37ab96399c2cf9d9b3f6b1cf5a1220857bbc
Tyler Jones <tjjones93@gmail.com>
2021-04-28 17:48:13 -0400

    init Graph component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Shape of Seinfeld</h1>
+    <Graph />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Graph from './components/Graph.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Graph
   }
 }
 </script>
