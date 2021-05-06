bfa3ace77e27d668b8de2ce7c90a6796dc29fe91
Tyler Jones <tjjones93@gmail.com>
2021-05-06 14:22:31 -0400

    test x- and y-axis

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -17,7 +17,7 @@ export default {
     },
     methods: {
         async getSeinfeldData() {
-            const data = await d3.csv('/data/S01E01.csv');
+            const data = await d3.csv('/data/S01E03.csv');
             let compounds = [];
             data.forEach(item => {
                 if (!item.compound == "") {
@@ -31,10 +31,13 @@ export default {
             let width = document.querySelector('.canvas').clientWidth;
             let height = document.querySelector('.canvas').clientHeight;
             
-            const x = d3.scaleLinear().domain([0, data.length]).range([0, 712]);
-            const y = d3.scaleLinear().domain([-1,1]).range([300, 0])
+            const x = d3.scaleLinear().domain([data[0].index, data[data.length-1].index]).range([0, 712]); // range corresponds to width of .canvas element
+                                            // the domain needs to be the {index} values for the first and last items in data array
+            const y = d3.scaleLinear().domain([-1,1]).range([300, 0]) // range corresponds to height of .canvas element
+                                            // the [-1, 1] references the 
 
             const line = d3.line()
+                .curve(d3.curveBumpX)
                 .x(d => x(d.index))
                 .y(d => y(d.compound));
             
@@ -49,6 +52,25 @@ export default {
                 .attr("fill", "none")
                 .attr("stroke", "rgb(34 150 243)")
                 .attr("stroke-width", 1);
+            
+            const yAxis = d3.axisRight()
+                            .scale(y);
+
+            const xAxis = d3.axisBottom(x)
+                            
+                            
+            
+            svg.append('g')
+                .attr("transform", "translate(0,10)")
+                .call(yAxis)
+
+            svg.append('g')
+                .attr('transform', 'translate(0, 160)')
+                .call(xAxis)
+
+            svg.append('line')
+                .attr('x1', 0)
+                .attr('x2', 100)
 
             console.log(compounds)
         }
@@ -60,7 +82,7 @@ export default {
 .canvas {
     height: 300px;
     width: 50%;
-    border: solid 1px #2c3e50;
+    /* border: solid 1px #2c3e50;*/ 
     margin: 0 auto;
 }
 
