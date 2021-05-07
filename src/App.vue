de9a06a9a97bc276130e85124863c47fdd6117eb
Tyler Jones <tjjones93@gmail.com>
2021-05-07 14:01:41 -0400

    testing

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
   <div id="app">
-    <h1>Shape of Seinfeld</h1>
+    <Header />
     <Graph />
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
 import Graph from './components/Graph.vue'
 
 export default {
   name: 'App',
   components: {
+    Header,
     Graph
+  },
+  data() {
+    return {
+      message: 'hello'
+    }
+  },
+  created() {
+    this.sayHello()
+  },
+  methods: {
+    sayHello() {
+      console.log(this.message)
+    }
   }
 }
 </script>
