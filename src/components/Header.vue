027c80a67be0a9280b1ea856234cf29cc27dd548
Tyler Jones <tjjones93@gmail.com>
2021-05-07 14:01:22 -0400

    add logo

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="header">
+        <img src="/images/shape-of-seinfeld.png"/>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+text {
+    fill: red;
+}
+
+.big {
+    font-size: 3rem;
+}
+
+.small {
+    font-size: 2rem;
+}
+</style>
